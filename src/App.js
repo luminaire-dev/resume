da3ec325105b4dda4ca65c0d78d2478e956f7179
Leila Elkhalidy <leila.elkhalidy@gmail.com>
2021-03-19 11:16:29 -0600

    add resume content
    add the rest of the resume text

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import styled from 'styled-components';
 const ResumeWrapper = styled.div`
   margin 2rem 5rem ;
   padding: 2rem;
-  @media only screen and (max-width: 650px) {
-    margin 1rem 2rem ;
+  @media only screen and (max-width: 550px) {
+    margin 1rem;
+    padding: 1rem;
   }
   border-radius: 0.5rem;
   opacity: 90%;
-  background-image: linear-gradient(purple, orange);
+  background-image: linear-gradient(purple, #e66327, purple);
   font-family: monospace;
 `
 
 const Description = styled.div`
-  letter-spacing: 0.3rem;
+  padding-bottom: 1rem;
+  letter-spacing: 0.20rem;
+
+  @media only screen and (max-width: 550px) {
+    letter-spacing: 0rem;
+  }
 `
 
 const ResumeBody = styled.div`
@@ -31,24 +37,49 @@ const ResumeBody = styled.div`
   }
 `
 
+const Heading = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding-top: 1rem;
+  @media only screen and (max-width: 550px) {
+    font-size: 1.25rem;
+  }
+`
+
+const SubHeading = styled.div`
+  font-size: 1rem;
+  font-weight: bold;
+  padding-top: ${props => props.noPadding ? 0 : "1rem"};
+`
+
 const Caret = styled.div`
   float: left;
   padding-right: 0.5rem;
-  font-size: 2rem;
+  font-size: 1.75rem;
+  @media only screen and (max-width: 550px) {
+    font-size: 1.25rem;
+    padding-right: 1rem;
+  }
 `
 
 const Title = styled.div`
-  font-size: 2rem;
+  font-size: 1.75rem;
   display: flex;
   overflow: hidden;
   white-space: nowrap;
-  border-right: 1.5rem transparent solid;
+  border-right: 1.25rem transparent solid;
+  @media only screen and (max-width: 550px) {
+    border-right: 1rem transparent solid;
+    font-size: 1.25rem;
+    max-width: 12rem;
+    font-weight: bold;
+  }
   animation: animated-text 1s steps(12,end) 1s 1 normal both,
-             animated-cursor 650ms steps(29,end) 5;
+             animated-cursor 700ms steps(29,end) 5;
 
   @keyframes animated-text{
     from{width: 0;}
-    to{width: 19rem;}
+    to{width: 16.5rem;}
   }
 
   @keyframes animated-cursor{
@@ -61,10 +92,12 @@ function App() {
   return (
     <div className="App-body">
       <ResumeWrapper>
-        <Caret>></Caret>
-        <Title>
-          Leila Elkhalidy
-        </Title>
+        <div>
+          <Caret>></Caret>
+          <Title>
+            Leila Elkhalidy
+          </Title>
+        </div>
         <ResumeBody>
           <Description>Full Stack Developer</Description>
 
@@ -74,27 +107,26 @@ function App() {
           }
 
 
-            <h2>Hi there, I'm Leila. 👋</h2>
+            <Heading>Hi there, I'm Leila. 👋</Heading>
             <p>I create simple, elegant solutions for complex problems. I started my coding journey at age 10 when I discovered
             I could modify the source code to my neopets page. Fast forward to today: I’m building customer facing web and mobile
              banking solutions at ATB Financial. As the Lead Developer on the Business Banking Mobile project, I’m continuously
              striving to improve code quality, testing coverage, and delivery cycles to better serve our customers. I’m naturally
              curious, I love working on teams, and I’m one of those people who puts ketchup on their poutine. 🤭</p>
 
-            <h2>Tech Stack 📚</h2>
+            <Heading>Tech Stack 📚</Heading>
             <p>JavaScript/ES6, React, Redux, Bable, Webpack, Node.js, Express, Cordova, JSON, XML, CSS, SOAP, REST, Json Web Tokens (JWT),
             Python, Pytes, Jest, Experitest (Mobile test automation), MongoDB, Java, Git, GitLab, Bitrise, Jenkins, Bitbucket, auth0</p>
 
-            <h2>Development Practices 👩🏻‍💻</h2>
+            <Heading>Development Practices 👩🏻‍💻</Heading>
             <p>Agile/SCRUM/Kanban, Trunk Based Development, Continuous Integration (CI), Continuous Deployment (CD), Feature Toggling,
             Microservice Architecture, Unit Testing, Test Automation, Paired Programming, Code Reviews, Test Driven Development</p>
 
-            <h2>Experience 👔</h2>
-            <h4>Senior Software Developer, ATB Financial</h4>
-            <p>May 2013 - Present, Edmonton AB</p>
+            <Heading>Experience 👔</Heading>
+            <SubHeading>Senior Software Developer, ATB Financial</SubHeading>
+            <SubHeading noPadding>May 2013 - Present, Edmonton AB</SubHeading>
 
-            <h5> Business Banking Mobile  - Lead Developer, 2018 - present </h5>
-              <li>Business Banking Mobile - Lead Developer, 2018 - present</li>
+            <h4> Business Banking Mobile  - Lead Developer, 2018 - present </h4>
               <li>Converted ATB’s Business Online platform into an iOS and Android app using Cordova</li>
               <li>Developed biometric and 2FA authentication flows using JWT and Auth0</li>
               <li>Developed in-app remote cheque deposits (leveraging native device camera and image analysis technologies)</li>
@@ -107,7 +139,7 @@ function App() {
               <li>Performed diligent and constructive code reviews</li>
               <li>Mentored and provided knowledge transfers to new devs</li>
 
-            <h5> Online Banking, 2017 - 2018 </h5>
+            <h4>Online Banking, 2017 - 2018 </h4>
               <li>Full stack development of a React web applications using Redux, Babel, Webpack, ES6, and Node.js</li>
               <li>Developed responsive, accessible, and reusable react components</li>
               <li>Developed e-transfer, bill payment, and account summary modules</li>
@@ -117,6 +149,38 @@ function App() {
               <li>Built API test using Pytest</li>
               <li>Configured CI/CD pipelines in GitLab and BitRise</li>
 
+            <h4>Retail Banking Mobile, 2016 - 2017</h4>
+              <li>Development of ATB's retail banking mobile and web platform using HTML5 and Cordova</li>
+              <li>Developed remote cheque deposit, e-transfer, bill payment, and account summary modules</li>
+              <li>Configured CI/CD pipelines in Jenkins</li>
+              <li>Tech sed: HTML5, CSS, Bootstrap, SVN, Jenkins, SAP Mobile Platform, SOAP</li>
+
+            <h4>ATB Flexible Pension Plan, 2015 - 2016</h4>
+            <li>Developed an internal .NET app to allow ATB team members to update or make changes to their pension</li>
+            <li>Tech sed: C#, ASP.NET, SQL Server, SOAP, SVN, NUnit.</li>
+            <li>ATB Customer Service Application (CSA), 2013 - 2015</li>
+            <li>Development and operational support of an internal desktop app used by our customer service teams in ATB branches.</li>
+            <li>Tech Used: Java (Swing), Object Oriented Programming (OOP), Eclipse, DB2, Axis2, SOAP, J2EE, SVN, CVS, Apache Ant.</li>
+
+            <Heading>Education 👩🏻‍🎓</Heading>
+            <h4>Digital Media and IT Diploma - Northern Alberta Institute of Technology (NAIT)</h4>
+            <p><b>Capstone Project</b>: Remodled and rebuilt an Access 97 database in SQL using relational databse practices like Private/Foreign keys and stored proceedures. Our Client was Alberta Health Services.</p>
+
+            <Heading>Other</Heading>
+            <h3>ATB Engineering Culture Excellence Award Winner, 2021</h3>
+            <p>Awarded to ATB team members who have gone above and beyond to exemplify ATB’s Engineering Culture.</p>
+
+            <h3>ATB Hackday - Fan Favourite Winner, 2019</h3>
+            <p>Developed a plugin that standardizes APIs creation at ATB, speeding up dev time and improving code quality and logging.</p>
+
+            <h3>HackEDbeta at Startup Edmonton, 2017</h3>
+            <p>Volunteered as a Mentor at this beginner-friendly hackathon.</p>
+
+            <h3>Apple Worldwide Developers Conference (WDDC), 2017</h3>
+            <p>Attended sessions on Apple Pay, Swift, Accessibility, Core ML, and AR.</p>
+
+            <h3>Google Cloud Relay, 2018</h3>
+            <p>In this 6 hours codingelay event, I wrote a Google Cloud hosted React app that provides a localized ski condition forecast.</p>
         </ResumeBody>
       </ResumeWrapper>
     </div>
