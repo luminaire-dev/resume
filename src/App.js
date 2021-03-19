58b52486954995cee15e28ed90a65f639d54212e
Leila <leila.elkhalidy@gmail.com>
2021-03-18 22:26:20 -0600

    add css typing animation

--- src/App.js
+++ src/App.js
@@ -2,34 +2,46 @@ import './App.css';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+  align: right;
+  padding: 2rem;
   font-family: monospace;
-  display: flex;
-  flex-direction: row;
+`
+
+const ResumeBody = styled.div`
+  padding: 2rem;
+  opacity: 0;
+  animation: fadeIn ease 3s;
+  animation-fill-mode: forwards;
+  animation-delay: 2s;
+
+  @keyframes fadeIn {
+    0% {opacity:0;}
+    100% {opacity:1;}
+  }
 `
 
 const Caret = styled.div`
-  Flex: 0;
-  padding: 0 1rem;
+  float: left;
+  padding-right: 0.5rem;
 `
 
 const Title = styled.div`
-  flex: 1;
   overflow: hidden;
   white-space: nowrap;
-  margin: 0 auto;
-  letter-spacing: .1em; /* Adjust as needed */
-  animation: typing 5s steps(30, end), blink .75s step-end infinite;
-  border-right: 1rem solid orange;
-  padding-right: 0.25rem;
+  letter-spacing: .1em;
+  padding-right: 0.15rem;
+  border-right: 1rem transparent solid;
+  animation: animated-text .95s steps(12,end) 1s 1 normal both,
+             animated-cursor 650ms steps(29,end) 5;
 
-  @keyframes typing {
-      from { width: 0% }
-      to { width: 100% }
+  @keyframes animated-text{
+    from{width: 0;}
+    to{width: 10.5rem;}
   }
 
-  @keyframes blink {
-      from, to { border-color: transparent; }
-      50% { border-color: orange; }
+  @keyframes animated-cursor{
+    from{border-color: pink;}
+    to{border-color: transparent);}
   }
 `
 
@@ -39,8 +51,12 @@ function App() {
       <header className="App-header">
         <Wrapper>
           <Caret>></Caret>
-          <Title>Leila Elkhalidy</Title>
-          <div></div>
+          <Title>
+            Luminaire
+          </Title>
+          <ResumeBody>
+            [insert resume body here]
+          </ResumeBody>
         </Wrapper>
       </header>
     </div>
