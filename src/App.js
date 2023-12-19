bf5a4aa8717ce87edcb40a07cce048529a700f8b
Leila Elkhalidy <leila@Leilas-MacBook-Pro.local>
2023-12-19 14:43:22 -0800

    add socials links to main page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './global.css';
 import React, { useState } from 'react';
 import CmdWindow from './cmd-window.js';
+import linkedinIcon from './linkedin.png';
+import gitHubIcon from './github.png';
+import emailIcon from './email.png';
 import Nav from './nav.js';
 import { WhoAmiCmd } from './globals.js';
 import styled from 'styled-components';
@@ -13,6 +16,15 @@ const Wrapper = styled.div`
   transform: translate(-50%, -50%);
 `
 
+const IconLinks = styled.img`
+  width: 2.5rem;
+  padding: 0.5rem;
+
+  &:hover {
+    filter: sepia(1) saturate(6000%) hue-rotate(290deg) brightness(106%) contrast(124%);
+  }
+`
+
 const App = () => {
   const [activeNavItem, setActiveNavItem] = useState(WhoAmiCmd);
   return (
@@ -20,6 +32,17 @@ const App = () => {
       <Wrapper>
         <CmdWindow activeNavItem={activeNavItem} />
         <Nav activeNavItem={activeNavItem} setActiveNavItem={setActiveNavItem} />
+        <div>
+          <a href="https://github.com/luminaire-dev" target="_blank" rel="noopener noreferrer" title="Github">
+            <IconLinks src={gitHubIcon} alt="Github" href="https://github.com/luminaire-dev" />
+          </a>
+          <a href="https://www.linkedin.com/in/leila-elkhalidy-928046212/" target="_blank" rel="noopener noreferrer" title="LinkedIn">
+            <IconLinks src={linkedinIcon} alt="Linkedin" />
+          </a>
+          <a href="mailto:leila.elkhalidy@gmail.com" title="Email">
+            <IconLinks src={emailIcon} alt="Email" />
+          </a>
+        </div>
       </Wrapper>
     </div>
   );
