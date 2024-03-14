323bd50cc6e8deeeb10d9ed242c2e3d0366214c2
Leila Elkhalidy <leila@Leilas-MacBook-Pro.local>
2024-03-14 13:59:57 -0700

    add remaining experience content

--- src/cmd-window.js
+++ src/cmd-window.js
@@ -6,7 +6,7 @@ import { colors } from './themes.js';
 
 const CLIWrapper = styled.div`
   width: 61rem;
-  height: 37rem;
+  height: 38rem;
   padding: 2rem;
   border-radius: 0.75rem;
   border: 1px solid ${colors.green};
@@ -27,15 +27,16 @@ const Body = styled.div`
   }
 `
 
+const Link = styled.a`
+  color: ${colors.green};
+`
+
 const Description = styled.div`
   padding-left: 1rem;
   opacity: 0;
   animation: fadeIn ease 1.6s;
   animation-fill-mode: forwards;
   animation-delay: 1.6s;
-  & > a {
-    color: ${colors.green};
-  }
 `
 
 const animateText = (length) => keyframes`
@@ -151,26 +152,26 @@ const whoami = () => {
           <Description>
              I create simple, elegant solutions for complex problems. With over 10 years of experience, 
              I bring a wealth of knowledge and skills gained through pivotal roles in two leading fintech companies&mdash;
-              <a href="https://www.koho.ca/" target="_blank" rel="noopener noreferrer">KOHO</a>
-            , <a href="https://www.atb.com/personal/" target="_blank" rel="noopener noreferrer">ATB Financial</a>. 
+              <Link href="https://www.koho.ca/" target="_blank" rel="noopener noreferrer">KOHO</Link>
+            , <Link href="https://www.atb.com/personal/" target="_blank" rel="noopener noreferrer">ATB Financial</Link>. 
           </Description>
           <br />
           <Description>
-             In my latest role at KOHO, I designed and developed backend solutions using Golang, across various areas of the business. 
-             I took a lead role in mission-critical initiatives, including the development of the Secured Credit Building feature, the 
-             enhancement and tech overhaul of KYC and fraud detection on the platform, and the modernization of the tech landscape with 
-             a migration from monolithic to microservice architecture. 
+            In my latest role at KOHO, I designed and developed backend solutions using Golang, across various areas of the business. 
+            I took a lead role in mission-critical initiatives, including the development of the Secured Credit Building feature, the 
+            enhancement and tech overhaul of fraud detection and KYC (Know Your Customer) in the app, and the modernization of the tech landscape with
+            new microservices in lieu of monolithic systems. 
           </Description>
 
           <Heading>Tech Stack</Heading>
           <Description>
             Golang, JavaScript(ES6), React, Redux, Bable, Webpack, Node, Express, PostgreSQL, Docker, GitHub, Git, DataDog, AWS,
-            Terraform, Cordova/Ionic, Jenkins, GitLab, Bitbucket, Bitrise, Auth0, JSON, XML, CSS, REST,  Python, Java, C#.
+            Terraform, Cordova/Ionic, Jenkins, RabbitMQ, GitLab, Bitbucket, Bitrise, Auth0, JSON, XML, CSS, REST,  Python, Java, C#.
           </Description>
 
           <Heading>Dev Practices</Heading>
           <Description>
-            CI/CD, Feature Toggling, Microservice Architecture, Event-Driven Architecture(Pub/Sub), Unit Testing, Integration Testing, Test Automation, 
+            CI/CD, Feature Toggling, Microservice Architecture, Accessibility, Event-Driven Architecture(Pub/Sub), Unit Testing, Integration Testing, Test Automation, 
             Test Driven Development, Observability, Paired Programming, Code Reviews, Agile, Scrum, Kanban, Documentation, Request for Comments(RFC), Mentoring & Feedback.
           </Description>
         </Body>
@@ -180,36 +181,51 @@ const whoami = () => {
 
 const otherthings = () => {
   return (
-      <Body>
-        some cool other things about me
-      </Body>
+    <Body>
+      Just a few other things about me:
+      <ul>
+        <li>I live in the beautiful Okanagan and love to ski, bike, climb and swim.</li>
+        <li>I make piano covers of video game music I enjoy. Check out my&nbsp; 
+          <Link href="https://www.youtube.com/channel/UCrOn-19Q386KEzQU8l4tipw" target="_blank" rel="noopener noreferrer" title="Github">youtube channel
+          </Link>
+        .</li>
+        <li>I'm that person who organizes the office the soccer team.</li>
+        <li>I could drink Chai lattes everyday until I die.</li>
+        <li>If you'd like to know anyting else just ask!</li>
+      </ul>
+    </Body>
   );
 }
 
 const community = () => {
   return (
       <Body>
-        In any Community is important to me... community envolvement:
+        Building community, beyond my regular day-to-day responsibilities, has always been important and meaningful to me. 
+        Here are some of the ways I've been involved and acknowledged for my participation in different communities:
+            <Heading>Women Who Go Toronto, 2022 - Present</Heading>
+            <div>I've been involved in this community of women and non-binary Go developers;
+              organizing book clubs, coding challenges, and talks on various Go topics.
+            </div>
+            
+            <Heading>deCODE Hackathon Fall, 2021</Heading>
+            <div>
+              Volunteered as a mentor, representing KOHO at this student hackathon where we built an expense splitting feature. 
+            </div>
 
-        <SubHeading>fuzz</SubHeading>
-        
-            <SubHeading>Women Who Go 2022 - present </SubHeading>
-            <div> organizer </div>
-
-            <SubHeading>ATB Eng Culture Excellence Award Winner, 2021</SubHeading>
+            <Heading>ATB Eng Culture Excellence Award Winner, 2021</Heading>
             <div>Awarded to ATB team members who have gone above and beyond to exemplify ATB’s Engineering Culture.</div>
 
-            <SubHeading>ATB Hackday - Fan Favourite Winner, 2019</SubHeading>
-            <div>Developed a plugin that standardizes APIs creation at ATB, speeding up dev time and improving code quality and logging.</div>
+            <Heading>ATB Hackday - Fan Favorite Winner, 2019</Heading>
+            <div>Participated in this 2 day hackathon where we developed a plugin that standardizes APIs 
+              creation at ATB, speeding up dev time and improving code quality and logging.</div>
 
-            <SubHeading>HackEDbeta at Startup Edmonton, 2017</SubHeading>
+            <Heading>Google Cloud Relay, 2018</Heading>
+            <div>Participated in this 6 hours coding relay challenge, where 
+              my team and I developed a Google Cloud hosted React app that provides a localized ski condition forecast.
+            </div>
+
+            <Heading>HackEDbeta at Startup Edmonton, 2017</Heading>
             <div>Volunteered as a Mentor at this beginner-friendly hackathon.</div>
-
-            <SubHeading>Apple Worldwide Developers Conference (WDDC), 2017</SubHeading>
-            <div>Attended sessions on Apple Pay, Swift, Accessibility, Core ML, and AR.</div>
-
-            <SubHeading>Google Cloud Relay, 2018</SubHeading>
-            <div>In this 6 hours coding relay challenge, I wrote a Google Cloud hosted React app that provides a localized ski condition forecast.</div>
       </Body>
   );
 }
--- src/experience.js
+++ src/experience.js
@@ -48,6 +48,13 @@ const Logo = styled.img`
   float: left;
 `
 
+
+const KohoLogo = styled.img`
+  width: 3.25rem;
+  margin: 0.9rem 1rem 0 0;
+  float: left;
+`
+
 const Experiene = () => {
   const [activeNavItem, setActiveNavItem] = useState('koho');
 
@@ -65,52 +72,90 @@ const Experiene = () => {
         <ExperienceSection>
          <Title>Sr. Backend Engineer - KOHO Financial  | 2021 - 2023</Title>         
          {/* 2021 - 2023*/ }
-         <Logo src={kohoLogo} alt="KOHO" />
+         <KohoLogo src={kohoLogo} alt="KOHO" />
           <CompanyDesc>KOHO is a Canadian fintech company that offers a Visa Card + mobile banking app 
             with modern features like credit building and helpful budgeting tools.</CompanyDesc>
           <SubHeading>Secured Credit Building (Jan 2023 - Aug 2023) </SubHeading>
               <ul>
                 <li>
-                  Designed and developed new backend microservice utilizing Golang, go-cron, Docker, AWS, DynamoDB, and Jenkins 
-                  to automate monthly collections of the subscription fees and loan repayments.
+                   Brought the Secured Credit Building feature from half-baked to release ready.
                 </li>
                 <li>
-                  Desined and developed new REST endpoints using Golang, go-cron, PostgreSQL, and RabbitMQ to 
-                  manager user's subscription to the feature.
+                  Developed a new microservice using Golang and DynamoDB to manage 
+                  collection of fee and payments from the users.
+                </li>
+                <li>
+                  Designed and developed REST endpoints using Golang and PostgreSQL to manage user's 
+                  subscription and fetch user's credit score from Equifax.
+                </li>
+                <li>
+                  Introduced event-driven patterns with RabbitMQ to replace legacy code orchestrating HTTPS calls to other services.
+                </li>
+                <li>
+                  Implemented a backend process to conduct experiments on varying price points
+                  using LaunchDarkly control/variant groups, and measuring user adoption rates in Metabase.
+                </li>
+                <li>
+                  Wrote unit and integration test.
+                </li>
+                <li>
+                  Provided on-call support for the feature and improved operational support processes.
+                </li>
+                <li>
+                  Worked closely with other developers, frequently pairing, mentoring, and performing code reviews.
+                </li>
+            </ul>
+
+            <SubHeading>KYC and Fraud Detection (2022 - 2023) </SubHeading>
+              <ul>
+                <li>
+                  Designed and developed a new microservice using Golang, PostgreSQL, Docker, AWS, and Jenkins to 
+                  handle KYC and fraud detection checks on user registration.
+                </li>
+                <li>Built integrations with various KYC and fraud detection tools including KOHO's machine learning model, Threatmetrix, 
+                  Comply Advantage, Trulioo and Onfido.
+                 </li>
+                 <li>Decommissioned legacy monolith code, re-routing traffic to new microservice, using event driven patterns.</li>
+                <li>Wrote documentation outlining the KYC and fraud check workflows.</li>
+                <li>
+                  Provided on-call support for KYC and Fraud detection features and contributed to operational support documentation.
+                </li>
+                <li>
+                  Wrote unit and integration test.
+                </li>
+                <li>
+                  Worked closely with other developers, frequently pairing, mentoring, and performing code reviews.
                 </li> 
+            </ul>
+
+            <SubHeading>Stripe Integration (2022) </SubHeading>
+              <ul>
                 <li>
-                  Implemented a backend process for conducting experiments with different price points, utilizing LaunchDarkly 
-                  flags, control/variant groups, and measuring outcomes in Metabase.
+                  Built new integration with Stripe, to allow users to load funds using other debit or credit cards.
                 </li>
-                <li>Developed a new REST endpoint using Golang, go-cron, PostgreSQL to fetch the user's credit score from Equifax every month. </li>
-                <li>Worked closely with other developers, frequently pairing, mentoring, and performing code reviews.</li>
-           
+                <li>
+                  Wrote documentation for the workflows.
+                </li>
+                <li>
+                  Wrote unit and integration test.
+                </li>
             </ul>
 
-            <SubHeading>Risk, KYC and Fraud (2022 - 2023) </SubHeading>
+            <SubHeading>Localization of backend content (2021 - 2022) </SubHeading>
               <ul>
-                <li>Built integrations with various fraud detection tools including threatmetrix, comply advantage, trulioo and onfido.
-                  Spun up a ne risk microservice 
-                  Modernized </li>
-                <li>New risk service, configuratio, pipeline using internal tool</li>
-                <li>Created the solution design and development plan.</li>
-                <li>Migrated monolith into new microservice</li>
-                <li>ML NODEL</li>
-                <li>integrated with comply advantage threatmetrix</li>
-                <li>built unit and intergation test</li>
-                <li>Mentored and paired with other developers and performed constructive code reviews.</li>
-                <li>documented entire kyc flow to date</li>
-                <li>Developed RESTful APIs using Express and microservices architecture.</li>
-            </ul>
-
-            <SubHeading>Stripe autoload (??? - ???) </SubHeading>
-              <ul>
-                <li>Built new endpoints integrating with stripe for a more seemless fund loading experine</li>
-            </ul>
-
-            <SubHeading>Localization of app (??? - ???) </SubHeading>
-              <ul>
-                <li>Built BACKEND serving the fe french or english content based on lcation. localization</li>
+                <li>Worked on a new backend Localization service that checks
+                   the user's language preference and feed french or english content accordingly. 
+                </li>
+                <li>
+                  Added Golang struct tags to all backend areas feeding content to the front end,
+                  so that they can be registered by the new service.
+                </li>
+                <li> 
+                  Wrote documentation for the new localization service. 
+                </li>
+                <li>
+                  Wrote unit and integration test.
+                </li>
             </ul>
         </ExperienceSection>
         )
@@ -122,7 +167,7 @@ const Experiene = () => {
           <Logo src={atbLogo} alt="ATB" />
           <CompanyDesc>ATB financial is a bank that provides financial services to over 800,000 Albertan residents and businesses.</CompanyDesc>
 
-            <SubHeading>Business Banking Mobile - Lead Developer (2018 - 2021) </SubHeading>
+            <SubHeading>Business Banking Mobile (2018 - 2021) </SubHeading>
               <ul>
                 <li>Converted ATB’s Business Online platform into an iOS and Android app using Cordova.</li>
                 <li>Created the Solution Design and Development Roadmap.</li>
@@ -137,7 +182,7 @@ const Experiene = () => {
                 <li>Worked closely with other developers, frequently pairing, mentoring, and performing code reviews.</li>
             </ul>
 
-            <SubHeading>Online Banking - Developer (2017 - 2018) </SubHeading>
+            <SubHeading>Online Banking (2017 - 2018) </SubHeading>
             <ul>
               <li>Developed a new online banking platform using React, Redux, Babel, Webpack, Express, and Node.js.</li>
               <li>Developed responsive, accessible, and reusable React components, following atomic design principles.</li>
@@ -148,20 +193,20 @@ const Experiene = () => {
               <li>Provided on-call support and particapted in outage resolution and deployments.</li>
             </ul>
 
-            <SubHeading>Retail Banking Mobile - Developer (2016 - 2017) </SubHeading>
+            <SubHeading>Retail Banking Mobile (2016 - 2017) </SubHeading>
             <ul>
               <li>Developed a new ATB mobile banking app using HTML5 and Cordova</li>
-              <li>Implemented account summary and bill payment UIs using HTML5, CSS, Bootstrap and SAP Mobile Platform</li>
-              <li>Built backend endpoints using Soap and XML</li>
-              <li>Used Jenkins to automate builds and deployments</li>
+              <li>Implemented account summary and bill payment UIs using HTML5, CSS, Bootstrap and SAP Mobile Platform.</li>
+              <li>Built backend endpoints using Soap and XML.</li>
+              <li>Used Jenkins to automate builds and deployments.</li>
               <li>Provided on-call support and particapted in outage resolution and deployments.</li>
             </ul>
 
-            <SubHeading>ATB Flexible Pension Plan - Developer (2015 - 2016)</SubHeading>
+            <SubHeading>ATB Flexible Pension Plan (2015 - 2016)</SubHeading>
             
             <ul>
-              <li>Developed an internal .NET app to allow ATB team members to update or make changes to their pension</li>
-              <li>Tech Used: C#, ASP.NET, SQL Server, SOAP, SVN, NUnit</li>
+              <li>Developed an internal .NET app to allow ATB team members to update or make changes to their pension.</li>
+              <li>Tech Used: C#, ASP.NET, SQL Server, SOAP, SVN, NUnit.</li>
             </ul>
 
             <SubHeading>ATB Customer Service Application - Jr. Developer (2013 - 2015)</SubHeading>
@@ -176,9 +221,11 @@ const Experiene = () => {
         <ExperienceSection>
           <Title>Northern Alberta Institute of Technology (NAIT) | 2011 - 2013</Title>
           <Logo src={naitLogo} alt="NAIT" />
-          <CompanyDesc>Digital Media and IT Diploma | 3.9 GPA
+          <CompanyDesc>Digital Media and IT (DMIT) Diploma | 3.9 GPA
             <div> Edmonton, Alberta</div>
           </CompanyDesc>
+
+          <div>Capstone: Modernized Alberta health services’s (AHS) legacy back-end to n-tier application.</div>
         </ExperienceSection>
         )
       }
